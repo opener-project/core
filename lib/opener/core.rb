ba6c209188feac43525c62947133c7763514b8f8
Yorick Peterse <yorickpeterse@olery.com>
2014-11-20 18:16:20 +0100

    Removed ErrorLayer class.
    This is being replaced by proper Rollbar/New Relic logging.

--- lib/opener/core.rb
+++ lib/opener/core.rb
@@ -3,11 +3,8 @@ require 'tempfile'
 require 'uri'
 require 'syslog'
 
-require 'nokogiri'
-
 require_relative 'core/version'
 require_relative 'core/argv_splitter'
 require_relative 'core/opt_parser'
 require_relative 'core/resource_switcher'
-require_relative 'core/error_layer'
 require_relative 'core/syslog'
