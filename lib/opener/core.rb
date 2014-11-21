259d05311b0d362b2a8c2c57554b77ead700e2f0
Yorick Peterse <yorickpeterse@olery.com>
2014-11-21 15:01:46 +0100

    Removed ArgvSplitter require.

--- lib/opener/core.rb
+++ lib/opener/core.rb
@@ -4,7 +4,6 @@ require 'uri'
 require 'syslog'
 
 require_relative 'core/version'
-require_relative 'core/argv_splitter'
 require_relative 'core/opt_parser'
 require_relative 'core/resource_switcher'
 require_relative 'core/syslog'
