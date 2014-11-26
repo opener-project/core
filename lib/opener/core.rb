26e44c8e17ac3d5166943cb3f345c6d965f47533
Yorick Peterse <yorickpeterse@olery.com>
2014-11-26 14:15:17 +0100

    Removed the OptParser class.
    This is no longer used/needed by any of the components now that Slop is put in
    place everywhere.

--- lib/opener/core.rb
+++ lib/opener/core.rb
@@ -8,6 +8,5 @@ require 'httpclient'
 require 'archive'
 
 require_relative 'core/version'
-require_relative 'core/opt_parser'
 require_relative 'core/resource_switcher'
 require_relative 'core/syslog'
